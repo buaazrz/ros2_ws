da7a929f88d9ebc9a5888cdb0d21acab1cbdd835
buaazrz <by2507139.@buaa.edu.cn>
2026-06-10 21:34:47 +0800

    modify  test_trajectory_controller_zrz

--- controllers/test/test_trajectory_franka.cpp
+++ controllers/test/test_trajectory_franka.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<rclcpp::Node>("test_trajectory_controller_zrz");
-  auto client = rclcpp_action::create_client<ACTION>(node, "CartesianTrajImpPDController/goal");
+  auto client = rclcpp_action::create_client<ACTION>(node, "CartesianTrajectoryController/goal");
 
   
 
